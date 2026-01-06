ba8871563d221afa157117741a076c31ae3be2c5
Artur5876 <artur.romanov@student.griffith.ie>
2026-01-06 22:02:07 +0000

    Added struct data structure for data initializing from sample_data.csv file and method for parsing data from that file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,35 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
+#include <chrono>
 
-// PostgreSQL headers (optional)
-#ifdef __has_include
-# if __has_include(<libpq-fe.h>)
-#  include <libpq-fe.h>
-#  define HAVE_POSTGRESQL 1
-# endif
-#endif
+struct Tick {
+    std::string symbol;
+    double price;
+    size_t volume;
+    long timestamp;
+};
 
-// Redis headers (optional)
-#ifdef __has_include
-# if __has_include(<hiredis/hiredis.h>)
-#  include <hiredis/hiredis.h>
-#  define HAVE_REDIS 1
-# endif
-#endif
-
+Tick parseCSVLine(const std::string& line) {
+    Tick tick;
+    //finding the first index in the line(after coma)
+    size_t pos1 = line.find(",");
+    size_t pos2 = line.find(",", pos1);
+    size_t pos3 = line.find(",", pos2);
+    size_t pos4 = line.find(",", pos3);
+    
+    tick.symbol =line.substr(0,pos1);
+    tick.price = std::stod(line.substr(pos2+1, pos2 -1 -pos1));
+    tick.volume = std::stoi(line.substr(pos3 + 1, pos3 - 1 - pos2));
+    tick.timestamp = std::stol(line.substr(pos4 + 1, pos4 - 1 - pos3));
+}
 int main() {
-    std::cout << "=== Simple C++ Project ===" << std::endl;
-    std::cout << "Built with: g++" << std::endl;
-    
-    #ifdef HAVE_POSTGRESQL
-    std::cout << "✓ PostgreSQL support available" << std::endl;
-    #else
-    std::cout << "✗ PostgreSQL headers not found" << std::endl;
-    #endif
-    
-    #ifdef HAVE_REDIS
-    std::cout << "✓ Redis support available" << std::endl;
-    #else
-    std::cout << "✗ Redis headers not found" << std::endl;
-    #endif
-    
-    std::cout << "\nReady to code!" << std::endl;
-    
+    std::cout << "=======================================\n";
+    std::cout << "MARKET DATA PIPELINE v0.1 - DAY 1\n";
+    std::cout << "=======================================\n\n";
+    auto currentTime = std::chrono::high_resolution_clock::now();
+    std::cout <<  "14.30 Initializing system...\n";
+
     return 0;
 }
